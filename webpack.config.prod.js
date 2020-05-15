4b9522958310ae538f55cbcf6080c4f0e9be254b
Anton Verchenko <anton.verch@gmail.com>
2020-05-15 23:10:49 +0300

    Add production configuration for Heroku

---
+++ webpack.config.prod.js
@@ -0,0 +1,45 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: ["./src/client/index.js"],
+    output: {
+        libraryTarget: "var",
+        library: "Client"
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCssAssetsWebpackPlugin({})]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: ["file-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/client/view/index.html",
+            filename: "./index.html"
+        }),
+        new MiniCssExtractPlugin({filename: 'main.css'}),
+        new WorkboxPlugin.GenerateSW()
+    ],
+}
