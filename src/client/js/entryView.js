61eb806ec2f2049df2e1fed418f81d5153b39f56
Anton Verchenko <anton.verch@gmail.com>
2020-05-16 16:17:27 +0300

    Adjust UI for bigger screens

--- src/client/js/entryView.js
+++ src/client/js/entryView.js
@@ -1,29 +1,33 @@
 /* Base HTML layout for trip entry*/
 function getEntryHtml(id) {
     return `<div class="entry" id="${id}">
-            <div class="image_holder">
-                <img class="trip_image">
-                <div class="image_cover">
-                    <h2 class="trip_name"></h2>
-                    <div class="trip_due_days"></div>
-                     <div class="weather_forecast">
-                        <div class="weather_temps">*</div>
-                        <div class="weather_icon"></div>
-                     </div>
+            <div class="left_panel">
+                <div class="image_holder">
+                    <img class="trip_image">
+                    <div class="image_cover">
+                        <h2 class="trip_name"></h2>
+                        <div class="trip_due_days"></div>
+                         <div class="weather_forecast">
+                            <div class="weather_temps">*</div>
+                            <div class="weather_icon"></div>
+                         </div>
+                    </div>
+                    <div class="entry_actions">
+                        <button type="button" class="button_save_entry">Save</button>
+                        <button type="button" class="button_delete_entry">Delete</button>
+                    </div>
                 </div>
-                <div class="entry_actions">
-                    <button type="button" class="button_save_entry">Save</button>
-                    <button type="button" class="button_delete_entry">Delete</button>
+                <div class="trip_info">
+                    <div class="city_search">
+                        <input type="text" class="city_search_input" autocomplete="off" placeholder="City">
+                    </div>
+                    <div class="dates_selection">
+                        <input class="date_from" type="date" placeholder="FROM">
+                        <input class="date_to" type="date" placeholder="FROM">
+                    </div>
                 </div>
             </div>
-            <div class="trip_info">
-                <div class="city_search">
-                    <input type="text" class="city_search_input" autocomplete="off" placeholder="City">
-                </div>
-                <div class="dates_selection">
-                    <input class="date_from" type="date" placeholder="FROM">
-                    <input class="date_to" type="date" placeholder="FROM">
-                </div>
+            <div class="right_panel">
                 <div class="package_items">
                     <div class="package_header">
                         <h3 class="package_title">Package items</h3>
